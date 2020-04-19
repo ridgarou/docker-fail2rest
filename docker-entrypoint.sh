60f9373427feb520e8ee3e0641904d0c160560cf
ridgarou <ridgarou@gmail.com>
2020-04-20 01:08:49 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,11 @@
 set -e
 
 # Create a default config
+
 if [ ! -f /srv/fail2rest/config.json ]; then
 	echo "fail2rest configuration initialization..."
-
 	mkdir -p /srv/fail2rest/
-    echo \
-		'{ "Addr": "0.0.0.0:5000", "Fail2banSocket": "/var/run/fail2ban/fail2ban.sock" }' \
-		> /srv/fail2rest/config.json
-
+	echo '{ "Addr": "0.0.0.0:5000", "Fail2banSocket": "/var/run/fail2ban/fail2ban.sock" }' > /srv/fail2rest/config.json
 	echo "fail2rest configuration generated"
 fi
 
